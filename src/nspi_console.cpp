53144625135586bb377551b80076b23d038698a1
Nontre12 <62849884+Nontre12@users.noreply.github.com>
2024-08-23 16:12:41 +0200

    Improved UI drawing performance by clearing output with consoleClear()

--- src/nspi_console.cpp
+++ src/nspi_console.cpp
@@ -7,9 +7,11 @@ nspi::Console::Console() { this->init(); }
 
 nspi::Console::~Console() { this->clean(); }
 
-void nspi::Console::update() { consoleUpdate(nullptr); }
+void nspi::Console::update() {
+  consoleUpdate(nullptr);
+  consoleClear();
+}
 
 void nspi::Console::init() { consoleInit(nullptr); }
 
 void nspi::Console::clean() { consoleExit(nullptr); }
-
