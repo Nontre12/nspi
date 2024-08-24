20040faa53c782e7725424fb1379e186dcb06354
Nontre12 <62849884+Nontre12@users.noreply.github.com>
2024-08-24 16:24:50 +0200

    Added title menu for being able to see full title info

--- src/nspi_app.cpp
+++ src/nspi_app.cpp
@@ -10,20 +10,9 @@ nspi::App::App() : quit(false) { this->init(); }
 
 nspi::App::~App() { this->clean(); }
 
-void nspi::App::init() {
-  socketInitializeDefault();
+void nspi::App::init() { socketInitializeDefault(); }
 
-  this->menu.push(new TitleSelectorMenu{pad});
-}
-
-void nspi::App::clean() {
-  while (!this->menu.empty()) {
-    delete this->menu.top();
-    this->menu.pop();
-  }
-
-  socketExit();
-}
+void nspi::App::clean() { socketExit(); }
 
 bool nspi::App::shouldClose() const { return this->quit || !appletMainLoop(); }
 
@@ -43,16 +32,11 @@ void nspi::App::handleInput() {
   }
 
   // delegate input to current menu after critical input checks
-  if (!this->menu.empty()) {
-    this->menu.top()->handleInput();
-  }
+  this->menuManager.handleInput();
 }
 
 void nspi::App::draw() const {
-  if (!this->menu.empty()) {
-    this->menu.top()->draw();
-  }
-
+  this->menuManager.draw();
   this->console.update();
 }
 
---
+++ src/nspi_menu_manager.cpp
@@ -0,0 +1,37 @@
+#include "nspi_menu_manager.h"
+
+#include "nspi_title_selector_menu.h"
+
+nspi::MenuManager::MenuManager::MenuManager(Pad &pad) : pad(pad) { this->init(); }
+
+nspi::MenuManager::MenuManager::~MenuManager() { this->clean(); }
+
+void nspi::MenuManager::init() { this->menus.push(new TitleSelectorMenu(*this, this->pad)); }
+
+void nspi::MenuManager::clean() {
+  if (!this->menus.empty() && this->menus.size() != 1) {
+    delete this->menus.top();
+    this->menus.pop();
+  }
+}
+
+void nspi::MenuManager::enter(nspi::Menu *menu) { this->menus.push(menu); }
+
+void nspi::MenuManager::back() {
+  if (!this->menus.empty() && this->menus.size() != 1) {
+    delete this->menus.top();
+    this->menus.pop();
+  }
+}
+
+void nspi::MenuManager::handleInput() {
+  if (!this->menus.empty()) {
+    this->menus.top()->handleInput();
+  }
+}
+
+void nspi::MenuManager::draw() const {
+  if (!this->menus.empty()) {
+    this->menus.top()->draw();
+  }
+}
